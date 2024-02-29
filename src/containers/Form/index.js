300b6b056892c175207a2dbe62605da0450062dd
ceddevstudent89 <cedstudent89@gmail.com>
2024-02-29 16:13:01 +0100

    Correction de bug

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -4,7 +4,13 @@ import Field, { FIELD_TYPES } from "../../components/Field";
 import Select from "../../components/Select";
 import Button, { BUTTON_TYPES } from "../../components/Button";
 
-const mockContactApi = () => new Promise((resolve) => { setTimeout(resolve, 1000); })
+const mockContactApi = () =>
+  new Promise((resolve) => {
+    setTimeout(resolve, 1000);
+    // Pour l'exemple, on simule un succès après 1 seconde
+    // En cas d'erreur simulée :
+    // setTimeout(() => reject(new Error('Une erreur est survenue')), 1000);
+  });
 
 const Form = ({ onSuccess, onError }) => {
   const [sending, setSending] = useState(false);
@@ -16,9 +22,11 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        /* Correction :  Appeler onSuccess après le succès de l'envoi */
+        onSuccess();
       } catch (err) {
         setSending(false);
-        onError(err);
+        onError(err); // Appeler onError en cas d'erreur
       }
     },
     [onSuccess, onError]
@@ -56,11 +64,11 @@ const Form = ({ onSuccess, onError }) => {
 Form.propTypes = {
   onError: PropTypes.func,
   onSuccess: PropTypes.func,
-}
+};
 
 Form.defaultProps = {
   onError: () => null,
   onSuccess: () => null,
-}
+};
 
 export default Form;
