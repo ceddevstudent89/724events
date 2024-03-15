0a6fe6aa7796c46901517b9c171bb85271f826b4
ceddevstudent89 <cedstudent89@gmail.com>
2024-03-15 11:29:12 +0100

    Bug slider bug trier les évènement, bug sur les dots

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,12 +7,24 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  const byDateDesc = data?.focus.sort(
+    (evtA, evtB) =>
+      // En utilisant .sort de cette façon, nous pouvons trier les events par ordre décroissant
+      new Date(evtB.date) - new Date(evtA.date)
   );
+
+  // console.log("Par date de description", byDateDesc); // ok c'est décroissant par date
+
+  // Exemple :
+  // const numbers = [5, 2, 9, 1, 5, 6];
+  // numbers.sort((a, b) => b - a); // Tri décroissant
+  // console.log(numbers); // Affiche [9, 6, 5, 5, 2, 1]
+
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () =>
+        // Ne pas oubliez length - 1 fin du tableau
+        setIndex(byDateDesc && index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
   };
@@ -22,9 +34,11 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        // La key doit être unique pour chaque élement
+        <div key={event.title}>
           <div
-            key={event.title}
+            // mauvais emplacement de la key={event.title}
+            // key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -42,15 +56,21 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`${_.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  // Les dots ne fonctionne pas
+                  // utiliser l'index du tableau byDateDesc
+                  // index === à l'image (cover)
+                  checked={index === radioIdx}
+                  // checked={idx === radioIdx}
+                  // Erreur console
+                  onChange={() => setIndex(index)}
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
